6b524f0e1fb2e0b87d8431c561b1c6d8f24bc7b4
Javier de Martín Gil <javierdemartin@me.com>
2020-04-11 19:26:13 +0200

    Added tests to cover Open Data & my API

--- BicisTests/BicisTests.swift
+++ BicisTests/BicisTests.swift
@@ -10,6 +10,8 @@ import XCTest
 
 class BicisTests: XCTestCase {
 
+    let remoteDataManager = DefaultRemoteDataManager()
+
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
@@ -21,6 +23,7 @@ class BicisTests: XCTestCase {
     func testExample() throws {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+        XCTAssertTrue(true)
     }
 
     func testPerformanceExample() throws {
@@ -30,11 +33,93 @@ class BicisTests: XCTestCase {
         }
     }
 
-    func parseDataFromApi() {
+    /// Test if the API endpoint for all of the available ciites exists and the parsing is done correctly
+    func testApiOpenDataEndpoints() {
 
+        for (city, _) in availableCities {
+
+            print("> Testing \(city)")
+            let expectation = XCTestExpectation.init(description: city)
+
+            remoteDataManager.getStations(city: city, completion: { parseResult in
+                // Got data from the api
+                switch parseResult {
+
+                case .success(let data):
+
+                    guard let randomStation = data.randomElement() else {
+                        return XCTFail("Couldn't get a random station")
+                    }
+
+                    XCTAssert(randomStation.id.count > 0)
+
+                    XCTAssert(randomStation.freeBikes >= 0)
+                    XCTAssert(randomStation.freeRacks >= 0)
+
+                    XCTAssert(randomStation.stationName.count >= 0)
+
+                    expectation.fulfill()
+
+                case .error(let error):
+                    XCTFail(error.localizedDescription)
+                }
+
+            })
+
+            self.wait(for: [expectation], timeout: 20)
+        }
+    }
+
+    /// Test my API and check if it
+    func testPredictionAvailabilityApi() {
+
+        for (city, cityData) in availableCities {
+
+            print("> Testing \(city)")
+            let expectation = XCTestExpectation.init(description: city)
+
+            remoteDataManager.getStations(city: city, completion: { parseResult in
+                // Got data from the api
+                switch parseResult {
+
+                case .success(let data):
+
+                    guard let randomStation = data.randomElement() else {
+                        return XCTFail("Couldn't get a random station")
+                    }
+
+                    var queryID = ""
+
+                    if city == "Bilbao" {
+                        queryID = randomStation.stationName
+                    } else {
+                        queryID = randomStation.id
+                    }
+
+                    self.remoteDataManager.getAllDataFromApi(city: cityData.apiName, station: queryID, completion: { allApiResult in
+                        switch allApiResult {
+
+                        case .success(let allData):
+                            XCTAssertTrue(allData.values.prediction.count == Int(Constants.lengthOfTheDay))
+                            XCTAssertTrue(allData.values.today.count >= 0)
+
+                            dump(allData.values.prediction)
+
+                            expectation.fulfill()
+
+                        case .error(let error):
+                            XCTFail(error.localizedDescription)
+                        }
+                    })
+
+                case .error(let error):
+                    XCTFail(error.localizedDescription)
+                }
+
+            })
+
+            self.wait(for: [expectation], timeout: 20)
+        }
     }
 
 }
-
-// TODO: Leer defaults a ver si se guarda lo de las compras
-// TODO: Leer version actual de defaults
