943ff3d25abbdf398d08c8cee89495b57cc202f9
Javier de Martín Gil <javierdemartin@me.com>
2020-05-17 21:31:36 +0200

    Stopped tracking files

--- Pods/Target Support Files/Pods-Bicis-BicisUITests/Pods-Bicis-BicisUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Bicis_BicisUITests : NSObject
-@end
-@implementation PodsDummy_Pods_Bicis_BicisUITests
-@end
--- Pods/Target Support Files/Pods-Bicis/Pods-Bicis-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Bicis : NSObject
-@end
-@implementation PodsDummy_Pods_Bicis
-@end
